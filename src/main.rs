1f0826d005f677b0b31ddd3f7232eb46deac870c
chrism <chrismichelmajor@gmail.com>
2019-11-13 11:51:48 -0700

    Added Philosopher struct per the Rust Language book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+//--------------------------------------------------------------------------------------------
+//
+//      EELE 565 - Parallel Processing
+//      Fall 2019
+//      Taught by Dr. Ross Snider       
+//
+//      Developers: James Eaton, Kaveen Liyanage, Chris Major, Trey Scofield
+//
+//      Objective: simulate the Dining Philosopher's Problem using the concurrency features of
+//      Rust.     
+//
+//--------------------------------------------------------------------------------------------
+
+
+// STRUCTS
+//--------------------------------------------------------------------------------------------
+
+// Create a struct for a Philosopher
+struct Philosopher {
+    name: String,                           // String to contain the Philosopher's name
 }
+
+// Implementation of a Philosopher
+impl Philosopher {
+
+    // Function to define the name for each philosopher
+    fn new(name: &str) -> Philosopher {
+
+        Philosopher {
+            name: name.to_string(),         // Assign the name 
+        }
+
+    }
+
+}
+
+
+// MAIN FUNCTION
+//--------------------------------------------------------------------------------------------
+fn main() {
+    
+    // Create five philosophers, per the original problem
+    let ph1 = Philosopher::new("Socrates");
+    let ph2 = Philosopher::new("Plato");
+    let ph3 = Philosopher::new("Kant");
+    let ph4 = Philosopher::new("Locke");
+    let ph5 = Philosopher::new("Descartes");        
+
+}
+
+
+//--------------------------------------------------------------------------------------------
+// END OF CODE
